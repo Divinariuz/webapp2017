8fd7fb717e84923dcca6f07c4977d06a8b43d297
Divinariuz <b6099@oic.jp>
2017-10-19 00:28:05 +0900

    first commit

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/61ahVGjnKZL._SL1008_.jpg',
+            'title' => '10Gtek for Cisco SFP-10G-SR, 10Gb/s SFP+ Transceiver module, 10GBASE-SR, MMF, 850nm, 300-meter',
+            'description' => 'For Cisco, 10GBASE-SR Module supports a link length of 26m on standard Fiber Distributed Data Interface (FDDI)-grade multimode fiber (MMF). Using 2000MHz*km MMF (OM3), up to 300m link lengths are possible. Using 4700MHz*km MMF (OM4), up to 400m link lengths are possible.',
+            'price' => 19.98
+        ]);
+        $product->save();
+
+    }
+}
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+          crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
+
+    @yield('styles')
+</head>
+<body>
+@include('partials.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
+<script
+        src="https://code.jquery.com/jquery-3.2.1.js"
+        integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+        crossorigin="anonymous"></script>
+<script
+        src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
+
+@yield('scripts')
+</body>
+</html>
