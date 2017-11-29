2e2ff48b433895c4422b45932a38ff693a5e41ed
Divinariuz <b6099@oic.jp>
2017-11-29 18:06:44 +0900

    title edited

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ route('shop.index') }}">ジェンジェンキンキン寿司寿司</a>
+            <a class="navbar-brand" href="{{ route('shop.index') }}">SFP STORE</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
